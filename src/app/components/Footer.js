9878f8ac14cce43b807d2cd71961e30c232f17a4
testudonavis <contactmikepark@gmail.com>
2023-12-05 17:30:58 -0800

    footer padding, main link hover styling, picture blur

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,7 +3,7 @@ import Socials from './Socials';
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col items-center p-2 sm:pt-5 sm:pb-5 sm:pr-10 sm:pl-10 text-tertiary-text">
+        <footer className="flex flex-col items-center pb-2 pt-10 sm:pt-10 sm:pb-1 sm:pr-10 sm:pl-10 text-tertiary-text">
             <Socials />
             <p>© 2023. michael park</p>
         </footer>
